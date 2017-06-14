5f38be480185e6b2a72ba576f1a569d6a8ee7442
vinigomes <vinigomes@outlook.com>
2017-06-14 11:45:17 -0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+var url = require('url');
+var request = require('request');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    var receitaws = 'https://www.receitaws.com.br/v1/cnpj/';
+    var params = url.parse(req.url, true).query;
+    var cnpjParam = params.cnpj;
+    var cnpj = '';
+    if (cnpjParam.includes('.') || cnpjParam.includes('/') || cnpjParam.includes('-')) {
+        cnpj = cnpjParam.replace('.', '');
+        cnpj = cnpj.replace('/', '');
+        cnpj = cnpj.replace('-', '');
+    } else {
+        cnpj = cnpjParam;
+    }
+    var queryReceitaws = receitaws+cnpj;
+    request(queryReceitaws, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var info = JSON.parse(body);
+            console.log(info);
+            res.status(200).send(info);
+        }
+    });
+});
+
+module.exports = router;
