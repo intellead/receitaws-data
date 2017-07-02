e2bd1fad16b96fc3dce6e0d44c699f6300b49d69
vinigomes <vinigomes@outlook.com>
2017-07-01 19:29:13 -0700

    debug with console

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ router.get('/', function(req, res, next) {
     var receitaws = 'https://www.receitaws.com.br/v1/cnpj/';
     var params = url.parse(req.url, true).query;
     var cnpjParam = params.cnpj;
+    cnpjParam = cnpjParam.toString();
     console.log("cnpjParam: " + cnpjParam);
     if (cnpjParam == '' || cnpjParam == undefined) {
         return res.render('index', { title: 'receitaws-data' });
