0611045248a5c24914914bcc112391faf5dc3b8e
vinigomes <vinigomes@outlook.com>
2017-07-01 19:23:58 -0700

    debug with console

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 var url = require('url');
 var request = require('request');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
     var receitaws = 'https://www.receitaws.com.br/v1/cnpj/';
     var params = url.parse(req.url, true).query;
     var cnpjParam = params.cnpj;
+    console.log("cnpjParam: " + cnpjParam);
     if (cnpjParam == '' || cnpjParam == undefined) {
         return res.render('index', { title: 'receitaws-data' });
     }
@@ -19,7 +19,9 @@ router.get('/', function(req, res, next) {
     } else {
         cnpj = cnpjParam;
     }
+    console.log("cnpj: " + cnpjParam);
     var queryReceitaws = receitaws+cnpj;
+    console.log("queryReceitaws: " + queryReceitaws);
     request(queryReceitaws, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var info = JSON.parse(body);
