cfb90183105d5112d30ef9d3a51c93dfbf89e0f5
vinigomes <vinigomes@outlook.com>
2017-07-01 19:39:28 -0700

    fix a bug with replace

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,6 @@ router.get('/', function(req, res, next) {
     var receitaws = 'https://www.receitaws.com.br/v1/cnpj/';
     var params = url.parse(req.url, true).query;
     var cnpjParam = params.cnpj;
-    console.log("cnpjParam: " + cnpjParam);
     if (cnpjParam == '' || cnpjParam == undefined) {
         return res.render('index', { title: 'receitaws-data' });
     }
@@ -19,13 +18,10 @@ router.get('/', function(req, res, next) {
     } else {
         cnpj = cnpjParam;
     }
-    console.log("cnpj: " + cnpj);
     var queryReceitaws = receitaws+cnpj;
-    console.log("queryReceitaws: " + queryReceitaws);
     request(queryReceitaws, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var info = JSON.parse(body);
-            console.log(info);
             res.status(200).send(info);
         }
     });
